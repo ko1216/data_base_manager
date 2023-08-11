3bb7e7cef269d4ce8ff70d91e9e3114c2e518957
korchaginks <kkonstantinserg@gmail.com>
2023-08-11 23:32:05 +0300

    Прописываю логику файл мейн с одновременным написанием недостающих классов и методов для выполнения задачи программы - менеджер DB

--- main/main.py
+++ main/main.py
@@ -1,16 +1,22 @@
+from src.config import config
 from src.employers_request_manager import EmployersRequestManager
+from src.database_classes import DBCreator
 
 import os
 
-path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src/employers_ids')
+employers_ids_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src/employers_ids')
+database_ini_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src/database.ini')
+params = config(database_ini_path)
 
 
 def main():
-    employers_request_manager = EmployersRequestManager(path)
+    employers_request_manager = EmployersRequestManager(employers_ids_path)
     employers_request_manager.vacancies_writer()
     vacancies = employers_request_manager.all_vacancies
 
-    DBCreator()
+    database = DBCreator('hh_vacancies', params)
+    database.create_database()
+    database.create_tables()
 
 
 if __name__ == '__main__':
