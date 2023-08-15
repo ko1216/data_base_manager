c8a1093d2586a0d2654418025d9a2f73ef6e1990
korchaginks <kkonstantinserg@gmail.com>
2023-08-15 09:49:24 +0300

    добавил аргумент url_params для уточнения запроса к API

--- src/request_funcs.py
+++ src/request_funcs.py
@@ -1,15 +1,15 @@
 import requests
 
 
-def get_headhunter_request(employer_id: str) -> dict:
+def get_headhunter_request(url_params=None) -> dict:
     """
     Функция делает запрос к HeadHunter через url запрос, возвращает словарь вакансий работадателя по переданному в юрл айди
-    :param employer_id: айди работодателя
+    :param url_params: параметры запроса
     :return: словарь с вакансиями
     """
-    response = requests.get('https://api.hh.ru/vacancies?employer_id=%s' % employer_id)
+    response = requests.get('https://api.hh.ru/vacancies', params=url_params)
     response_json = response.json()
     return response_json
 
-#
+
 # print(get_headhunter_request('1060266'))
