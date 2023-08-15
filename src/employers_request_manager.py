82ec44f092e2e083a1a8999d8b512bb09004ccbd
korchaginks <kkonstantinserg@gmail.com>
2023-08-15 09:48:50 +0300

    Добавил параметры поиска вакансий по айди компаний, а также параметр максимального запроса вакансий до 2000

--- src/employers_request_manager.py
+++ src/employers_request_manager.py
@@ -23,14 +23,30 @@ class EmployersRequestManager:
 
         return employers
 
-    def vacancies_writer(self) -> None:
+    def save_vacancies(self) -> None:
         """
         Метод делает запросы через API HeadHunter на сайт для получения информации о вакансиях работадателей по их id
         После этого передает информацию о всех вакансиях работадателей из списка в общий список - атрибут класса
         """
         self.all_vacancies.clear()
+        page = 0
+        employers_ids = []
+        for id in self.read_file().values():
+            employers_ids.append(id)
 
-        for employer_id in self.read_file().values():
-            vacancies = get_headhunter_request(employer_id)['items']
-            self.all_vacancies.extend(vacancies)
+        params = {'text': 'python',
+                  'area': '1',
+                  'page': str(page),
+                  'per_page': '100',
+                  'only_with_salary': True,
+                  'employer_id': employers_ids
+                  }
 
+        for page in range(0, 20):
+            vacancies = get_headhunter_request(params)['items']
+            self.all_vacancies += vacancies
+
+
+# emp = EmployersRequestManager('employers_ids')
+# emp.save_vacancies()
+# print(emp.all_vacancies)
